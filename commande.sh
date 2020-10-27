6e3be1b1112021f86d4569f1f5ababa165c30792
iattach <memoryinwinter@outlook.com>
2020-10-27 12:04:59 +0100

    shell update

--- commande.sh
+++ commande.sh
@@ -5,3 +5,15 @@ eval `opam config env`
 export OCAMLRUNPARAM=b
 
 make
+
+if [ $# != 4 ]
+then
+    echo "Usage: 1:source 2:begin 3:end 4:output"
+else 
+    ./ftest.native $1 $2 $3 $4
+    
+    `dot -Tsvg $4.dot > $4.svg`
+fi
+
+
+
